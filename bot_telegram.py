a3652c3ffc5f03c15ba01a234ee1ca965fd9904f
Alex <alexos977@yandex.ru>
2022-10-22 23:56:01 +1000

    new

--- bot_telegram.py
+++ bot_telegram.py
@@ -1,12 +1,20 @@
+import os
+
 from aiogram.utils import executor
+import config
 from create_bot import dp
 from data_base import sqlite_db
+from create_bot import bot
 
 
 
-async def on_startup(_):
+
+async def on_startup(dp):
     print('Бот вышел в онлайн')
-    sqlite_db.sql_start()
+    await bot.set_webhook(config.URL_APP)
+
+async def on_shutdown(dp):
+    await bot.delete_webhook()
 
 from handlers import client, admin, other
 
@@ -14,4 +22,11 @@ client.register_handlers_clients(dp)
 admin.register_handlers_admin(dp)
 other.register_handlers_other(dp)
 
-executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
+executor.start_webhook(
+    dispatcher=dp,
+    webhook_path="",
+    on_startup=on_startup,
+    on_shutdown=on_shutdown,
+    skip_updates=True,
+    host="0.0.0.0",
+    port=int(os.environ.get('PORT', 5000)))
--- config.py
+++ config.py
@@ -1,1 +1,2 @@
 TOKEN = '5467368679:AAE_7Tq0x-r0RKwNlGc_zD7zhJCkeLWYzXI'
+URL_APP = "https://pizzakroogsterbot.herokuapp.com/"
