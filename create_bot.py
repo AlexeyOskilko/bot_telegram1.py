0ecc1d69f08fde15eb05b96966ad2c1ecc03d6d0
Alex <alexos977@yandex.ru>
2022-10-22 23:06:38 +1000

    123

--- create_bot.py
+++ create_bot.py
@@ -6,11 +6,10 @@ import os, hashlib
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.types import InputTextMessageContent, InlineQueryResultArticle
-import config
 
 storage=MemoryStorage()
 
-bot = Bot(token=config.TOKEN)#os.getenv('TOKEN'))
+bot = Bot(token=os.getenv('TOKEN'))
 dp = Dispatcher(bot,storage=storage)
 
 
