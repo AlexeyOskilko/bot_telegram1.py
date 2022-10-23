70523e49feeddf2831e349321d3559519a1c4582
Alex <alexos977@yandex.ru>
2022-10-24 01:35:41 +1000

    requirements update

--- data_base/sqlite_db.py
+++ data_base/sqlite_db.py
@@ -7,8 +7,8 @@ base = sq.connect('pizza_cool.db')
 cur = base.cursor()
 
 
-    if base:
-        print('Data base connected OK!')
+if base:
+    print('Data base connected OK!')
     base.execute('CREATE TABLE IF NOT EXISTS menu(img TEXT,name TEXT PRIMARY KEY, description TEXT, price TEXT)')
     base.commit()
 
